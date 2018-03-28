8c9035d3042eebb792b84ff0540e7e914804f344
Harsh Patel <harshpatel40@yahoo.com>
2018-03-28 16:58:06 -0400

    maps2 added arrays with all info

--- maps2.py
+++ maps2.py
@@ -3,6 +3,8 @@ Simple Program to help you get started with Google's APIs
 """
 import urllib.request
 import json
+from tkinter import *
+
 #Google MapsDdirections API endpoint
 endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
 api_key = 'AIzaSyAtnTpeJXirzSS7CRGaIntlXDcJ6V14EGM'
@@ -17,14 +19,53 @@ response = urllib.request.urlopen(request).read()
 #Loads response as JSON
 directions = json.loads(response.decode('utf-8'))
 print(directions)
+
 #print(directions.keys())
-routes = directions['routes']
+
 #print(routes[0].keys())
-legs = routes[0]['legs']
+legs = directions['routes'][0]['legs']
 print("\n")
 #iterate through the steps and print the html instructions (which are the directions) print distance for length of each step
 stepslen= len(legs[0]['steps'])
+print("number of steps: " + str(stepslen-1))
+step_duration_list = []
+step_startloc_lat_list = []
+step_startloc_lng_list = []
+step_endloc_lat_list = []
+step_endloc_lng_list = []
+step_html_list= []
+step_maneuver_list = []
+
 for x in range(stepslen):
-	print(legs[0]['steps'][x]['html_instructions'])
+	a=legs[0]['steps'][x]['duration']['text']
+	step_duration_list.append(a)
+	a=legs[0]['steps'][x]['start_location']['lat']
+	step_startloc_lat_list.append(a)
+	a=legs[0]['steps'][x]['start_location']['lng']
+	step_startloc_lng_list.append(a)
+	a=legs[0]['steps'][x]['end_location']['lat']
+	step_endloc_lat_list.append(a)
+	a=legs[0]['steps'][x]['end_location']['lng']
+	step_endloc_lng_list.append(a)
+	a=legs[0]['steps'][x]['html_instructions']
+	step_html_list.append(a)
+
+	if x==0:
+		step_maneuver_list.append('none')
+	else:
+		a=legs[0]['steps'][x]['maneuver']
+		step_maneuver_list.append(a)	
+
+
+	# print the array you want
+	#print(str(x)+":"+ step_duration_list[x])
+	#print(str(x)+":"+ str(step_startloc_lat_list[x]))
+	#print(str(x)+":"+ str(step_startloc_lng_list[x]))
+	#print(str(x)+":"+ str(step_endloc_lat_list[x]))
+	#print(str(x)+":"+ str(step_endloc_lng_list[x]))
+	#print(str(x)+":"+ step_html_list[x])
+	#print(str(x)+":"+ step_maneuver_list[x])
+
 #total distance of trip
-print(legs[0]['distance']['text'])
+
+print('total trip distance: ' + legs[0]['distance']['text'])
