4d76b03fbbb43ab64d0717996cdb2f916e124755
Harsh Patel <harshpatel40@yahoo.com>
2018-04-07 17:06:25 -0400

    updated so function returns one integer value

--- speed.py
+++ speed.py
@@ -7,28 +7,35 @@ import simplejson as json
 #python3 speed.py 40.503197 -74.385920 500
 
 def maxspeed(coordinates, radius):
-    lat, lon = coordinates
-    api = overpy.Overpass()
+	lat, lon = coordinates
+	api = overpy.Overpass()
 
 # fetch all ways and nodes
-    result = api.query("""
-            way(around:""" + radius + """,""" + lat  + """,""" + lon  + """) ["maxspeed"];
-                (._;>;);
-                    out body;
-                        """)
-    results_list = []
-    for way in result.ways:
-        road = {}
-        road["name"] = way.tags.get("name", "n/a")
-        road["speed_limit"] = way.tags.get("maxspeed", "n/a")
-        nodes = []
-        for node in way.nodes:
-            nodes.append((node.lat, node.lon))
-        road["nodes"] = nodes
-        results_list.append(road)
-    return results_list
+	result = api.query("""
+			way(around:""" + radius + """,""" + lat  + """,""" + lon  + """) ["maxspeed"];
+				(._;>;);
+					out body;
+					    """)
+	results_list = []
+	returnspeed = 25
+	for way in result.ways:
+		road = {}
+		road["name"] = way.tags.get("name", "n/a")
+		road["speed_limit"] = way.tags.get("maxspeed", "n/a")
+		# i deleted the coordinates of the speed limits  
+		#nodes = []
+		#for node in way.nodes:
+		#    nodes.append((node.lat, node.lon))
+		#road["nodes"] = nodes
+		results_list.append(road)
+		# return just one value
+	if results_list:
+		returnspeed = (results_list[0]['speed_limit']).split(' ')[0]
+
+	return returnspeed
 # returning only one speed limit
 # do something where if there is no speed limit within 100 m of gps then return 0 to indicate no speed
 
 results = maxspeed((sys.argv[1], sys.argv[2]), sys.argv[3])
-print(json.dumps(results))
+print(results)
+#print(json.dumps(results))
